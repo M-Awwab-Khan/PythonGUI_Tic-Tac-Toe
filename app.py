a2ec360d79c0c0d915408a6f8cb21d0db54a1aeb
Awwab <mawwabkhank2006@gmail.com>
2023-03-23 23:02:29 +0500

    Fixed: Result Text displaying awkwardly

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ class Tic_Tac_Toe():
             color = 'gray'
 
         self.canvas.delete("all")
-        self.canvas.create_text(size_of_board / 2, size_of_board / 3, font="cmr 60 bold", fill=color, text=text)
+        self.canvas.create_text(size_of_board / 2, size_of_board / 3, font="cmr 60 bold", width=500 ,fill=color, text=text, justify="center")
 
         score_text = 'Scores \n'
         self.canvas.create_text(size_of_board / 2, 5 * size_of_board / 8, font="cmr 40 bold", fill=Green_color,
@@ -99,7 +99,7 @@ class Tic_Tac_Toe():
 
         score_text = 'Player 1 (X) : ' + str(self.X_score) + '\n'
         score_text += 'Player 2 (O): ' + str(self.O_score) + '\n'
-        score_text += 'Tie                    : ' + str(self.tie_score)
+        score_text += 'Tie               : ' + str(self.tie_score)
         self.canvas.create_text(size_of_board / 2, 3 * size_of_board / 4, font="cmr 30 bold", fill=Green_color,
                                 text=score_text)
         self.reset_board = True
